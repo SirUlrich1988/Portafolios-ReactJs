06d2b2e9b4745390a327776785e8864f4450533d
Luis Vallejo <luisern29@gmail.com>
2022-12-30 18:59:21 -0500

    prototipo de about

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import '../styles/aboutMe.css'
 
 const AboutMe = () => {
   return (
-    <div>
-      <h1>ESTA ES LA PAGINA ACERCA DE MI</h1>
+    <div className='about-container'>
+      <div className='about-img'>
+        <img className='profile-img' src="https://img.freepik.com/vector-premium/perfil-avatar-hombre-icono-redondo_24640-14044.jpg" alt="imagen de perfil" />
+      </div>
+      <div className='about-info'>
+        <div className='about-box'>
+          <span className='about-data'>
+            Soy una persona comprometida con mi trabajo, me gustan los desafíos nuevos que exigen perseverancia de mi parte para lograrlos, tengo facilidad para relacionarme con las personas.
+          </span>
+          <span className='about-data'>
+            En los lugares que pretendo trabajar siempre dare lo mejor de mi buscando
+            todos los dias aprender y dar el 100% con el fin de que mi trabajo sirva
+            para el exito de mi equipo de trabajo, mio y de mi empresa.
+            Todo lo que logro aprender siempre busco perfecionarlo con el fin de
+            generar una mejora que pueda facilitar y mejorar el desempeño de mis
+            actividades.
+          </span>
+        </div>
+
+      </div>
+
     </div>
   )
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import '../styles/home.css'
 
 const Home = () => {
   return (
-    <div>
-      <h1>ESTA ES LA PAGINA ACERCA DE INICIO</h1>
+    <div className='home-container'>
+      <div className='home-box'>
+        <h2 className='home-welcome'>Hola Bienvenido</h2>
+        <h2 className='home-name'>Soy Luis Vallejo</h2>
+        <h1 className='home-title'>Desarrollador Full Stack</h1>
+      </div>
+      
     </div>
   )
 }
