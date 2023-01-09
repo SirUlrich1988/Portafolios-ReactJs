dffd84e8aeb5512e6c40964565b14fa7eea139ed
Luis Vallejo <luisern29@gmail.com>
2023-01-09 00:30:51 -0500

    seccion contacto terminada

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -5,25 +5,23 @@ const AboutMe = () => {
   return (
     <div className='about-container'>
       <div className='about-img'>
-        <img className='profile-img' src="https://img.freepik.com/vector-premium/perfil-avatar-hombre-icono-redondo_24640-14044.jpg" alt="imagen de perfil" />
+        <img className='profile-img' src="../public/img/perfil.png" alt="imagen de perfil" />
       </div>
       <div className='about-info'>
         <div className='about-box'>
           <span className='about-data'>
             Soy una persona comprometida con mi trabajo, me gustan los desafíos nuevos que exigen perseverancia de mi parte para lograrlos, tengo facilidad para relacionarme con las personas.
-          </span>
+          </span><br />
           <span className='about-data'>
             En los lugares que pretendo trabajar siempre dare lo mejor de mi buscando
             todos los dias aprender y dar el 100% con el fin de que mi trabajo sirva
-            para el exito de mi equipo de trabajo, mio y de mi empresa.
-            Todo lo que logro aprender siempre busco perfeccionarlo con el fin de
-            generar una mejora que pueda facilitar y optimizar el desempeño de mis
+            para el exito de mi equipo y de mi empresa.
+            En todo lo que logro aprender siempre busco perfeccionarlo con el fin de
+            crear una mejora que pueda facilitar y optimizar el desempeño de mis
             actividades.
           </span>
         </div>
-
       </div>
-
     </div>
   )
 }
--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,9 +1,107 @@
-import React from 'react'
+import { Button } from 'bootstrap'
+import React, { useState } from 'react'
+import '../styles/contact.css'
 
 const Contact = () => {
+
+  const [input, setInput] = useState({})
+  const [messages, setMessage] = useState(null)
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    const email = 'luisern29@gmail.com'
+    // alert ('Enviado')
+    const URL_BASE = `https://formsubmit.co/ajax/${email}`
+
+    fetch(URL_BASE, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(input)
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data) {
+          setMessage('Tu mensaje se ha enviado correctamente')
+          setTimeout(() => {
+            setMessage(null)
+          }, 3000)
+        }
+      })
+      .catch(error => {
+        if (error) {
+          setMessage('Sucedio un error, intentalo mas tarde por favor')
+          setTimeout(() => {
+            setMessage(null)
+          }, 3000)
+        }
+      })
+    setInput({})
+  }
+
+  function handleChange({ name, value }) {
+    setInput({ ...input, [name]: value })
+  }
+
   return (
-    <div>
-      <h1>ESTA ES LA PAGINA ACERCA DE CONTACTO</h1>
+    <div className='contact-container'>
+      <div className='contact-box1'>
+        <img src="https://www.garzablancaresort.com.mx/blog/wp-content/uploads/2018/03/beach-safety.jpg" alt="mi ubicacion" />
+      </div>
+      <div className='contact-box2'>
+        <div className='contact-info'>
+          <h1>Contactame</h1>
+          <h5>Hola si tienes alguna idea personal o para tu negocio que quisieras que cobre vida puedes contactarme por este medio y sera un placer ayudarte.</h5>
+        </div>
+        <form className='contact-form' onSubmit={handleSubmit}>
+          <div className='contact-name'>
+            <label htmlFor="nombre">Nombre:</label><br />
+            <input
+              id='nombre'
+              type="text"
+              name='nombre'
+              value={input.nombre || ''}
+              onChange={(e) => handleChange(e.target)}
+              required
+            />
+          </div>
+          <div className='contact-email'>
+            <label htmlFor="correo">Correo:</label><br />
+            <input
+              id='correo'
+              type="email"
+              name='correo'
+              value={input.correo || ''}
+              onChange={(e) => handleChange(e.target)}
+              required
+            />
+          </div>
+          <div className='contact-message'>
+            <div className='message-box'>
+              <label htmlFor="mensaje">Mensaje:</label><br />
+              <textarea
+                id='mensaje'
+                name='mensaje'
+                value={input.mensaje || ''}
+                onChange={(e) => handleChange(e.target)}
+                cols="30"
+                rows="10">
+              </textarea>
+            </div>
+            <div className='input-box'>
+              <input
+                type="submit"
+                value="Enviar"
+              />
+              {
+                messages ?? <p>{messages}</p>
+              }
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
